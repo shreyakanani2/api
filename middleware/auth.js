0a8d9f409675b1f33ecf486f4cbdc6e021ce40d7
shreya <shreya.kanani@dalanguages.co.uk>
2023-05-21 23:55:13 +0100

    changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,7 +9,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
   let token;
 
   if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
-    token = req.headers.authorization.split("")[1];
+    token = req.headers.authorization.split(" ")[1];
   }
 
   if (!token) {
